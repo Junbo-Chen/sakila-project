dfcc376c355fc34cd20b870e11aceb12c4d86e6d
Junbo-Chen <junbochen@hotmail.com>
2025-09-20 22:33:44 +0200

    about page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { 
+    title: 'Home',
+    containerType: 'container'   // standaard
+  });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'About - Sakila Dashboard',
+    containerType: 'container-fluid'  // speciale layout
+  });
 });
 
 module.exports = router;
